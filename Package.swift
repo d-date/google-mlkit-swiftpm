5ce02b1ee4dfe3be3014b93ad7a39e37f20061d0
Daiki Matsudate <d.matsudate@gmail.com>
2022-08-16 00:01:30 +0900

    Remove unused package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/google/promises.git", from: "2.1.0"),
-    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.19.0"),
     .package(url: "https://github.com/google/GoogleDataTransport.git", from: "9.1.4"),
     .package(url: "https://github.com/google/GoogleUtilities.git", from: "7.7.1"),
     .package(url: "https://github.com/google/gtm-session-fetcher.git", from: "1.7.2"),
